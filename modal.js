c31713af3146b968adb384902a83205b4cdcfb8f
isaac <isaacsvianna@gmail.com>
2023-02-24 15:05:28 +0000

    feat: implementa modal para imagens dos projetos

---
+++ modal.js
@@ -0,0 +1,56 @@
+// Obtém as imagens da galeria
+const images = document.querySelectorAll('.gallery .project .slider img');
+const html = document.querySelector('html');
+
+// Obtém o modal e a imagem do modal
+const modal = document.querySelector('.modal');
+const modalImage = document.querySelector('.modal-image');
+
+// Obtém os botões de navegação
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+// Armazena a imagem atual e o índice da imagem na galeria
+let currentImage;
+let currentIndex;
+
+// Abre o modal quando uma imagem for clicada
+images.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    // Define a imagem atual e o índice da imagem na galeria
+    currentImage = image;
+    currentIndex = index;
+
+    // Atualiza a imagem no modal
+    modalImage.src = currentImage.src;
+
+    // Mostra o modal
+    html.style.overflow = 'hidden';
+    modal.style.display = 'block';
+  });
+});
+
+// Fecha o modal quando o botão de fechar for clicado
+const closeButton = document.querySelector('.close');
+closeButton.addEventListener('click', () => {
+  html.style.overflow = 'auto';
+  modal.style.display = 'none';
+});
+
+// Navega para a imagem anterior
+prevButton.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    currentImage = images[currentIndex];
+    modalImage.src = currentImage.src;
+  }
+});
+
+// Navega para a próxima imagem
+nextButton.addEventListener('click', () => {
+  if (currentIndex < images.length - 1) {
+    currentIndex++;
+    currentImage = images[currentIndex];
+    modalImage.src = currentImage.src;
+  }
+});
